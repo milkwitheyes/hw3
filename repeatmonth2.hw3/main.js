97ea42e8e839d6d85f8140cc0e9303e379f0e956
milkwitheyes <154975761+milkwitheyes@users.noreply.github.com>
2024-02-26 15:51:45 +0600

    Add files via upload

---
+++ repeatmonth2.hw3/main.js
@@ -0,0 +1,363 @@
+const obj = {
+    name: 'alihan',
+    last_name: 'asanbaev',
+    phone: '+996 772 21 84 66',
+    meried: false,
+    gender: 'male',
+    address: {
+        street: 'petrova',
+        number: 12,
+        country: 'KG',
+        region: 'osh',
+        city: 'osh'
+    },
+    frends: [
+        {
+            name: 'aliha',
+            last_name: 'borubaev',
+            phone: '+996 732 21 84 66',
+            meried: true,
+            gender: 'male',
+            address: {
+                street: 'petrova',
+                number: 12,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+        },
+        {
+            name: 'alihan',
+            last_name: 'borubaev',
+            phone: '+996 732 21 84 66',
+            meried: true,
+            gender: 'male',
+            address: {
+                street: 'petrova',
+                number: 12,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+        },
+        {
+            name: 'alihan',
+            last_name: 'borubaev',
+            phone: '+996 732 21 84 66',
+            meried: true,
+            gender: 'male',
+            address: {
+                street: 'petrova',
+                number: 12,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+            workplace: {
+                name: 'geeks',
+                address: {
+                    street: 'abdimatalip amatova',
+                    number: 2,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+            },
+        }
+    ],
+    workplace: {
+        name: 'geeks',
+        address: {
+            street: 'abdimatalip amatova',
+            number: 2,
+            country: 'KG',
+            region: 'osh',
+            city: 'osh'
+        },
+    },
+    famaly: [
+        {
+            skills: [
+                'ts', 'js', 'go', 'py'
+            ],
+
+            name: 'tima',
+            role: 'father',
+            last_name: 'usmanov',
+            phone: '+996 732 21 84 66',
+            meried: true,
+            gender: 'male',
+            address: {
+                street: 'petrova',
+                number: 12,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+        },
+        {
+            name: 'tima',
+            role: 'mother',
+            last_name: 'usmanov',
+            phone: '+996 732 21 84 66',
+            meried: true,
+            gender: 'male',
+            address: {
+                street: 'petrova',
+                number: 12,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+        },
+        {
+            name: 'tima',
+            last_name: 'usmanov',
+            role: 'sister',
+            phone: '+996 732 21 84 66',
+            meried: true,
+            gender: 'male',
+            address: {
+                street: 'petrova',
+                number: 12,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+
+        }
+    ]
+    ,
+    skills: [
+
+        'ts', 'js', 'go', 'py'
+    ]
+
+}
+// const name = obj.name;
+// const lastName = obj.last_name;
+// const phone = obj.phone;
+// const frends =obj.frends.name;
+
+// const address = obj.address.street + ', ' + obj.address.number + ', ' + obj.address.city + ', ' + obj.address.region + ', ' + obj.address.country ;
+
+
+// const outputHTML =
+//     ` <p>Name: ${name}</p> <p>Last Name: ${lastName}</p> <p>Phone: ${phone}</p> <p>Address: ${address}</p> <br/> 
+//     <p></p> ;`
+// document.getElementById('output').innerHTML = outputHTML;
+
+// console.log(obj);
+
+for (let i = 0; i < obj.frends.length; i++) {
+    const element = obj.frends[i];
+    
+}
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.frends);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.workplace);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+console.log(obj.famaly);
+
+
+
+
+
+
+
+
+
